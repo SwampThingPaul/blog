24f1af7254529ecefeb9888f3b9f09bd92be6c95
Paul Julian <pjulian@ufl.edu>
2018-12-15 20:31:35 -0500

    new post

--- rcode_knit.R
+++ rcode_knit.R
@@ -1,3 +1,5 @@
 
-#prettyjekyll::FormatPost("_knitr/knitr-minimal.Rmd")
+
 #prettyjekyll::FormatPost("_knitr/2017-06-14-old-inspiring-the-young.Rmd")
+#prettyjekyll::FormatPost("_knitr/2017-08-20-TaylorSlough.Rmd")
+#prettyjekyll::FormatPost("_knitr/knitr-minimal.Rmd")
