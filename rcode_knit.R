17260bbd07bc776dae7a4b9e3300bf6074535150
Paul Julian <pjulian@ufl.edu>
2018-12-17 13:31:57 -0500

    updating

--- rcode_knit.R
+++ rcode_knit.R
@@ -1,7 +1,21 @@
 
 
 #prettyjekyll::FormatPost("_knitr/2017-06-14-old-inspiring-the-young.Rmd")
+#file.remove("_knitr/2017-06-14-old-inspiring-the-young.html")
+
 #prettyjekyll::FormatPost("_knitr/2017-08-20-TaylorSlough.Rmd")
+#file.remove("_knitr/2017-08-20-TaylorSlough.html")
+
 #prettyjekyll::FormatPost("_knitr/2017-08-23-TaylorSlough.Rmd")
+#file.remove("_knitr/2017-08-23-TaylorSlough.html")
+
 #prettyjekyll::FormatPost("_knitr/2017-09-01-Pyrite.Rmd")
+#file.remove("_knitr/2017-09-01-Pyrite.html")
+
 #prettyjekyll::FormatPost("_knitr/knitr-minimal.Rmd")
+#file.remove("_knitr/knitr-minimal.html")
+
+#prettyjekyll::FormatPost("_knitr/2018-12-21-sciart.Rmd")
+#file.remove("_knitr/2018-12-21-sciart.html")
+
+#file.create("_knitr/test.Rmd")
